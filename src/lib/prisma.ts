2f473a5faca06c2e0cd2b3d33061fdb00cbddcc8
Stivan-Lucas <lucamargostivan@gmail.com>
2026-02-26 15:01:10 -0300

    chore: adicionados novos metodos de configuração

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,19 +3,19 @@ import { env } from "../utils/env";
 
 // 1. Definimos o tipo para o objeto global
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+	prisma: PrismaClient | undefined;
 };
 
 // 2. Instanciamos o prisma (reutilizando se já existir)
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    accelerateUrl: env.DATABASE_URL,
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		accelerateUrl: env.DATABASE_URL,
+		log:
+			env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+	});
 
 // 3. Em desenvolvimento, salvamos a instância no globalThis
 if (env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+	globalForPrisma.prisma = prisma;
 }
