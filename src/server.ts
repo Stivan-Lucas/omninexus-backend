d14d0cfbc933a38ba2ed259ff3a3497734d91623
Stivan-Lucas <lucamargostivan@gmail.com>
2026-02-26 09:39:38 -0300

    chore: teste biome

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,21 @@ import { env } from "./utils/env";
 import { logger } from "./utils/logger";
 
 const fastify = Fastify({
-	logger,
-	genReqId: () => crypto.randomUUID(),
+  logger,
+  genReqId: () => crypto.randomUUID(),
 });
 
 fastify.get("/", async (_request, _reply) => {
-	return { hello: "world" };
+  return { hello: "world" };
 });
 
 const start = async () => {
-	try {
-		await fastify.listen({ port: env.PORT, host: env.HOST });
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+  try {
+    await fastify.listen({ port: env.PORT, host: env.HOST });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
