5a90d8c74c8ffa91f22c5795eb47ff13213cacef
Stivan-Lucas <lucamargostivan@gmail.com>
2026-03-28 09:32:43 -0300

    chore(database): generate initial migration for users table

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'drizzle-kit'
-import { env } from './src/env/env'
 
 export default defineConfig({
   schema: './src/database/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL ?? '',
   },
 })
